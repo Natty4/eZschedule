7d9bd5d2ae503de628223e5e137776b774f95fee
natty4 <natty7kt@gmail.com>
2025-03-26 23:14:14 +0300

    Add initial files for Swagger UI and update admin assets

--- core/management/commands/create_superuser_if_not_exists.py
+++ core/management/commands/create_superuser_if_not_exists.py
@@ -1,17 +1,20 @@
 from django.core.management.base import BaseCommand
-from core.models import User
+from django.contrib.auth import get_user_model
 from django.conf import settings
 
 class Command(BaseCommand):
-    help = 'Create superuser if it does not exist'
+    help = 'Creates a superuser if it does not already exist'
 
-    def handle(self, *args, **kwargs):
-        username = 'admin'
-        email = 'admin@ezgo.com'
-        password = settings.SUPERUSER_PASSWORD  # Password from settings
+    def handle(self, *args, **options):
+        username = 'admin'  # Change this to the desired superuser username
+        email = 'admin@example.com'  # Change this to the desired email
+        password = settings.SUPERUSER_PASSWORD
 
+        User = get_user_model()
+
+        # Check if the superuser already exists
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
+            User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS(f'Superuser {username} created successfully'))
         else:
             self.stdout.write(self.style.SUCCESS(f'Superuser {username} already exists'))
--- goapp/settings.py
+++ goapp/settings.py
@@ -99,17 +99,6 @@ WSGI_APPLICATION = "goapp.wsgi.application"
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT', 5432),
-#     }
-# }
-
 
 
 if DEBUG:
