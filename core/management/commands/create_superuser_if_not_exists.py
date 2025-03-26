b769067bf0aff9119ad1ea1c83818daa019e3f50
natty4 <natty7kt@gmail.com>
2025-03-26 23:25:14 +0300

    Add logging to superuser creation command

--- core/management/commands/create_superuser_if_not_exists.py
+++ core/management/commands/create_superuser_if_not_exists.py
@@ -1,13 +1,17 @@
+import logging
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
+# Get the logger
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     help = 'Creates a superuser if it does not already exist'
 
     def handle(self, *args, **options):
-        username = 'admin'  # Change this to the desired superuser username
-        email = 'admin@example.com'  # Change this to the desired email
+        username = 'admin'  
+        email = 'admin@ezgo.com'  
         password = settings.SUPERUSER_PASSWORD
 
         User = get_user_model()
@@ -15,6 +19,8 @@ class Command(BaseCommand):
         # Check if the superuser already exists
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
+            logger.info(f'Superuser {username} created successfully')
             self.stdout.write(self.style.SUCCESS(f'Superuser {username} created successfully'))
         else:
+            logger.info(f'Superuser {username} already exists')
             self.stdout.write(self.style.SUCCESS(f'Superuser {username} already exists'))
